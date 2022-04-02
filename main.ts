8748aa7b4ee7fd83111bca31198b84f79af6ff37
Ibis Liven <ibis.liven@protonmail.com>
2022-04-01 23:23:51 -0400

    initial Word Limit commit

--- main.ts
+++ main.ts
@@ -1,137 +1,165 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface WordLimitSettings {
+	limit: number
+	type: string
 }
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: WordLimitSettings = {
+	limit: 200,
+	type: 'word'
 }
+const Types = new Map([
+	['line', {
+		value: 'line',
+		display: 'lines',
+		min: 5,
+		max: 100,
+		step: 5
+	}],
+	['word', {
+		value: 'word',
+		display: 'words',
+		min: 100,
+		max: 1000,
+		step: 50
+	}],
+	['char', {
+		value: 'char',
+		display: 'characters',
+		min: 1000,
+		max: 10000,
+		step: 500
+	}]])
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class WordLimit extends Plugin {
+	settings: WordLimitSettings
+
+	statusBar: HTMLElement
+	eventHandlers = new Map()
 
 	async onload() {
-		await this.loadSettings();
+		// load settings
+		await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// helper functions
+		const getViewData = (): string => {
+			const data = this.app.workspace.getActiveViewOfType(MarkdownView).getViewData()
+			const noYAML = (str: string): string => str.replace(/^---.*---/s, '')
+			const noLink = (str: string): string => str.replace(/(\[{2}.*\|)|(\]\(.*\))/g, '')
+			const noHTML = (str: string): string => str.replace(/<(.*)>.?|<(.*) \/>/g, '')
+			const noSymbols = (str:string): string => str.replace(/\P{Letter}/gu, '')
+			return noSymbols((noHTML(noLink(noYAML(data)))))
+		}
+		const getChars = (): number =>
+			/*	quick and dirty rough char count */
+			getViewData().replace(/\s+/g,'').length
+		const getWords = (): number =>
+			/* quick and dirty rough word count */
+			getViewData().split(/\s+/).length
+		const getLines = (): number =>
+			/* quick and dirty rough line count */
+			getViewData().split(/\r\n/).length
+		const getRemaining = (type: string, limit: number): number => {
+			switch (type) {
+				case 'char': return limit - getChars()
+				case 'word': return limit - getWords()
+				case 'line': return limit - getLines()
+		}}
+		const updateStatusBar = (statusBarItem: HTMLElement, remaining: number) =>
+			statusBarItem.setText(
+				Math.abs(remaining) + ' ' +
+				Types.get(this.settings.type).display + ' ' +
+				(Math.sign(remaining) === -1 ? 'over limit' : 'remaining')
+			)
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// add status bar
+		this.statusBar = this.addStatusBarItem()
+		updateStatusBar(this.statusBar, getRemaining(this.settings.type, this.settings.limit))
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// add event listeners
+		/* might change handler keys to include workspace or vault */
+		this.eventHandlers.set('editor-change', () => {
+			const remaining = getRemaining(this.settings.type, this.settings.limit)
+			if (remaining < 0)
+				new Notice(`you've gone over your ${Types.get(this.settings.type).display.slice(0, -1)} limit!`)
+			updateStatusBar(this.statusBar, remaining)
+		})
+		this.eventHandlers.set('active-leaf-change', () => {
+			const remaining = getRemaining(this.settings.type, this.settings.limit)
+			if (remaining < 0)
+				new Notice(`this file is over your ${Types.get(this.settings.type).display.slice(0, -1)} limit!`)
+			updateStatusBar(this.statusBar,remaining)
+		})
+		this.eventHandlers.forEach((value, name) =>
+			/* -@ts-ignore the overloads are getting confused by name being type string 
+			    @ts-ignore because they're defined with specific strings like 'quit' */
+			this.registerEvent(this.app.workspace.on(name, value)))
+		// add settings tab
+		this.addSettingTab(new SettingsTab(this.app, this))
 	}
 
-	onunload() {
-
-	}
+	async onunload() {
+		// remove event listeners
+		this.eventHandlers.forEach((value,name) =>
+			this.app.workspace.off(name,value)
+	)}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
-
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(this.settings)
+		// update status bar to match new settings
+		this.statusBar.setText(`${this.settings.limit} ${Types.get(this.settings.type).display} maximum`)
 	}
 }
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class SettingsTab extends PluginSettingTab {
+	plugin: WordLimit
+	limits: Setting
+	constructor(app: App, plugin: WordLimit) {
+		super(app, plugin)
+		this.plugin = plugin
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	setLimitSlider(): void {
+		/* allows for changing limit slider parameters after initial display */
+		this.limits
+			.clear()
+			.setName('limit')
+			.setDesc(`set your ${Types.get(this.plugin.settings.type).display.slice(0, -1)} limit`)
+			.addSlider(slider => slider
+				.setLimits(
+					Types.get(this.plugin.settings.type).min,
+					Types.get(this.plugin.settings.type).max,
+					Types.get(this.plugin.settings.type).step)
+				.setValue(Types.get(this.plugin.settings.type).min)
+				.setDynamicTooltip()
+				.onChange(async value => {
+					this.plugin.settings.limit = value
+					await this.plugin.saveSettings()
+				}))
 	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
 	display(): void {
-		const {containerEl} = this;
+		// clear old settings
+		this.containerEl.empty()
 
-		containerEl.empty();
+		// add type dropdown
+		new Setting(this.containerEl)
+			.setName('limit type')
+			.setDesc('set the type of limit')
+			.addDropdown(dropdown => dropdown
+				.addOption(Types.get('char').value, Types.get('char').display)
+				.addOption(Types.get('word').value, Types.get('word').display)
+				.addOption(Types.get('line').value, Types.get('line').display)
+				.setValue(Types.get(this.plugin.settings.type).value)
+				.onChange(async value => {
+					this.plugin.settings.type = value
+					this.plugin.settings.limit = Types.get(this.plugin.settings.type).min
+					this.setLimitSlider()
+					await this.plugin.saveSettings()
+				}))
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// add limit slider
+		this.limits = new Setting(this.containerEl)
+		this.setLimitSlider()
 	}
 }
